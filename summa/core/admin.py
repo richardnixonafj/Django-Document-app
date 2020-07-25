56fd0c15a8a7be5eeeb2f790bf70b2347da8475d
Richard Nixon <richardnixon@posteo.org>
2020-07-25 19:31:22 -0300

    new register and filter & fix .ignore

--- summa/core/admin.py
+++ summa/core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Filiado
 
-admin.site.Register(Filiado)
-
-# Register your models here.
+@admin.register(Filiado)
+class FiliadoAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'data_da_extracao',
+        'nome_do_partido',
+        'nome_do_filiado',
+        
+    )
+    search_fields=('nome_do_filiado',)
+    list_filter=('nome_do_partido',)
